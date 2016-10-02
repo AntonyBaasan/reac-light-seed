7f1c34a1d9ef1022b32bb8eae647af5b2e0ce8df
Antony Baasandorj <antony.baasan@gmail.com>
2016-10-01 22:34:49 -0500

    add courses page

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Link} from "react-router";
+
+class CoursesPage extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Course</h1>
+            </div>
+        );
+    }
+}
+
+export default CoursesPage;
